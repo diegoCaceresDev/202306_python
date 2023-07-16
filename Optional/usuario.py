5d2c37c6994269412c771f4ddc5f36a9e784f445
Druyeah <you@example.com>
2023-07-15 20:34:14 -0400

    Subo tarea practica de usario

---
+++ Optional/usuario.py
@@ -0,0 +1,49 @@
+class Usuario:
+    def __init__(self, name, ciudad="Encarnacion"):
+        self.nombre = name
+        self.ciudad = ciudad
+        self.monto_cuenta = 0
+
+    def hacer_deposito(self, monto):
+        self.monto_cuenta += monto
+
+    def hacer_retiro(self, monto):
+        self.monto_cuenta -= monto
+
+    def ver_balance(self):
+        print(f"El saldo actual de {self.nombre} es de: ${self.monto_cuenta}")
+        
+
+def hacer_tranferencia(monto, se_descuenta, aumenta):
+    se_descuenta.monto_cuenta -= monto
+    aumenta.monto_cuenta += monto
+
+
+Usuario1 = Usuario("Diego Caceres", "Encarnacion")
+Usuario2 = Usuario("Juan Perez", "Asuncion")
+Usuario3 = Usuario("Pedro Gonzalez", "CDE")
+
+Usuario1.hacer_deposito(100)
+Usuario1.hacer_deposito(400)
+Usuario1.hacer_deposito(150)
+Usuario1.hacer_retiro(300)
+Usuario1.ver_balance()
+
+Usuario2.hacer_deposito(300)
+Usuario2.hacer_deposito(500)
+Usuario2.hacer_retiro(200)
+Usuario2.hacer_retiro(300)
+Usuario2.ver_balance()
+
+Usuario3.hacer_deposito(1000)
+Usuario3.hacer_retiro(100)
+Usuario3.hacer_retiro(500)
+Usuario3.hacer_retiro(400)
+Usuario3.ver_balance()
+
+print("-"*40)
+
+hacer_tranferencia(150, Usuario1, Usuario3)
+Usuario1.ver_balance()
+Usuario3.ver_balance()
+
