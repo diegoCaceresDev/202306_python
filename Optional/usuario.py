36e6496dca4e1d890a2be32305d6eba381d26df8
Druyeah <you@example.com>
2023-07-17 22:15:48 -0400

    subo tarea encadenar metodos

--- Optional/usuario.py
+++ Optional/usuario.py
@@ -6,13 +6,17 @@ class Usuario:
 
     def hacer_deposito(self, monto):
         self.monto_cuenta += monto
+        return self
 
     def hacer_retiro(self, monto):
         self.monto_cuenta -= monto
+        return self
+
 
     def ver_balance(self):
         print(f"El saldo actual de {self.nombre} es de: ${self.monto_cuenta}")
-        
+        return self
+
 
 def hacer_tranferencia(monto, cuenta_a_descontar, cuenta_a_aumentar):
     cuenta_a_descontar.hacer_retiro(monto)
@@ -23,23 +27,11 @@ Usuario1 = Usuario("Diego Caceres", "Encarnacion")
 Usuario2 = Usuario("Juan Perez", "Asuncion")
 Usuario3 = Usuario("Pedro Gonzalez", "CDE")
 
-Usuario1.hacer_deposito(100)
-Usuario1.hacer_deposito(400)
-Usuario1.hacer_deposito(150)
-Usuario1.hacer_retiro(300)
-Usuario1.ver_balance()
+Usuario1.hacer_deposito(100).hacer_deposito(400).hacer_deposito(150).hacer_retiro(300).ver_balance()
 
-Usuario2.hacer_deposito(300)
-Usuario2.hacer_deposito(500)
-Usuario2.hacer_retiro(200)
-Usuario2.hacer_retiro(300)
-Usuario2.ver_balance()
+Usuario2.hacer_deposito(300).hacer_deposito(500).hacer_retiro(200).hacer_retiro(300).ver_balance()
 
-Usuario3.hacer_deposito(1000)
-Usuario3.hacer_retiro(100)
-Usuario3.hacer_retiro(500)
-Usuario3.hacer_retiro(400)
-Usuario3.ver_balance()
+Usuario3.hacer_deposito(1000).hacer_retiro(100).hacer_retiro(500).hacer_retiro(400).ver_balance()
 
 print("-"*40)
 
